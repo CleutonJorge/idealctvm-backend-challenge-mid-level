52ef270a6b9cb83963e1f0d7b2a159d11d74056d
cleuton <juniorcj_14@hotmail.com>
2023-02-05 15:12:32 -0300

    Adicionando projeto base springboot, mvc kotlin gradle

---
+++ src/main/kotlin/com/api/assettracking/AssetTrackingApplication.kt
@@ -0,0 +1,11 @@
+package com.api.assettracking
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AssetTrackingApplication
+
+fun main(args: Array<String>) {
+	runApplication<AssetTrackingApplication>(*args)
+}
